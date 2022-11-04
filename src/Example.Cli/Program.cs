197163f58c13f0cb90f1b809449a3f4d8d0540f3
Anton Prokofiev <antonkofa@gmail.com>
2022-11-04 01:14:55 +0100

    Add examples (#11)
    * add examples
    
    * bump version
    
    * add readme

---
+++ src/Example.Cli/Program.cs
@@ -0,0 +1,34 @@
+﻿using GerrKoff.Monitoring;
+using GerrKoff.Monitoring.MetricsUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+const string appName = "example-app";
+
+await Logging.RunSafeAsync(async () =>
+{
+    var config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", false)
+        .Build();
+
+    Logging.UseLoggingCli(config, new (appName));
+
+    var services = new ServiceCollection()
+        .AddLoggingCli()
+        .AddMetricsCli(config, new (appName))
+        .BuildServiceProvider();
+
+    var metricsCollector = services.GetRequiredService<IMetricsCollectorCli>();
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    var cancelationToken = new CancellationTokenSource(10*1000).Token;
+
+    var metricsCollectingTask = metricsCollector.Collect(cancelationToken);
+
+    Console.WriteLine("Hello, World!");
+
+    await metricsCollectingTask;
+
+    logger.LogInformation("App finished");
+}, () => "v1.0.0");
